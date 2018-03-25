6fc3c3d524842529821780a53541aabd274dfc03
uyi777 <168698141@qq.com>
2018-03-26 07:02:32 +0800

    1425

--- app/pages/personal/gogogo.js
+++ app/pages/personal/gogogo.js
@@ -6,7 +6,7 @@ import {
     Platform,
     TextInput,
     ScrollView,
-    StyleSheet,Alert,TouchableOpacity,Dimensions
+    StyleSheet,Alert,TouchableOpacity,Dimensions,FlatList
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 const {width,height}=Dimensions.get('window')
@@ -20,38 +20,54 @@ class Cart extends React.Component {
     componentDidMount(){
         Alert.alert("gogogo")
     }
-
-    render(){
+     
+    _renderItem = ({item,index}) => {
         const right=(<View style={{flexWrap:'wrap',flexDirection:'row',height:100,width:70,justifyContent:'space-between'}}>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>全</Text>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>大</Text>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>小</Text>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>奇</Text>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>偶</Text>
-            <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>清</Text>
-        </View>)
-        const center=(<View style={{flexWrap:'wrap',flexDirection:'row',flex:1,justifyContent:'space-between'}}>
-       { numbers.map((item,index)=>
-       <View style={{width:_width,height:_width,  alignItems: 'center',}} key={index}>
-             <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center',}} >{item}</Text>
-        </View>
-        )}
-    </View>)
-        return <ScrollView style={{flex:1}}>
-        <View style={[styles.container]}>
-        {/* <TouchableOpacity onPress={()=>this.props.navigation.navigate("tototo")}>
-            <Text>gogogo</Text>
-        </TouchableOpacity> */}
-      {  numbers.map((item,i)=>
-        <View style={{width:'100%',height:100,padding:10,flexDirection:'row',}}>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>全</Text>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>大</Text>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>小</Text>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>奇</Text>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>偶</Text>
+        <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center'}}>清</Text>
+    </View>);
+
+const center=(<View style={{flexWrap:'wrap',flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+{ numbers.map((item,index)=>
+<View style={{width:_width,height:_width,  alignItems: 'center',}} key={index}>
+      <Text style={{width:20,height:20,borderWidth:1,borderColor:'#000',borderRadius:10,textAlign:'center',}} >{item}</Text>
+ </View>
+ )}
+</View>)
+        return (
+            <View style={{width:'100%',height:100,padding:10,flexDirection:'row',}}>
                 <View style={{width:60}}>
                     <Text>万位</Text>
                 </View>
                 {center}
                 {right}
         </View>
-        )}
-       
+        )
+    }
+
+    render(){
+      
+    
+        return <ScrollView style={{flex:1}}>
+        <View style={[styles.container]}>
+        {/* <TouchableOpacity onPress={()=>this.props.navigation.navigate("tototo")}>
+            <Text>gogogo</Text>
+        </TouchableOpacity> */}
+     
+         <FlatList
+                    data={numbers}
+                    renderItem={this._renderItem}
+                    
+                    showsVerticalScrollIndicator={false}
+                    getItemLayout={(data,index)=>(
+                        {length: 100, offset: 100 * index, index}
+                        )}
+                    ref={(ref) => this._listRef = ref }
+                    />
         
     
         </View>
